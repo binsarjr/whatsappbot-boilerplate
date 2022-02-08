5ec409772d9b5f923e7c8d122c25e38a7cbfef4f
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-02-08 14:41:25 +0700

    add clear message schedule

---
+++ src/Adapter/v1/Tasks/ClearMessageTask.ts
@@ -0,0 +1,16 @@
+import { scheduleJob } from 'node-schedule'
+import v1 from '..'
+import { isProducation } from '../../../Infrastructure/Utils/validate'
+
+if (isProducation()) {
+    scheduleJob(
+        'clearAllMessage',
+        {
+            rule: '0 0 0 * * *',
+            tz: 'Asia/Jakarta'
+        },
+        async () => {
+            await v1().clear()
+        }
+    )
+}
